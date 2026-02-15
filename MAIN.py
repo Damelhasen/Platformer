d3db506af3f0960dd5fae0ea9be4edaa2546679d
Sheby <jotheexplorer123@gmail.com>
2026-02-14 19:35:27 -0500

    good ver

--- MAIN.py
+++ MAIN.py
@@ -1,10 +1,15 @@
 import importlib.util
 from ursina import *
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
+import random
+import time
 app = Ursina()
 camera.orthographic = True
 camera.fov = 10
 
+   
+
+platform = Entity(model='quad', y = 2, scale_x = 12 , collider = 'box' , color = color.rgba(0.89, 0.52, 1, 1), x = 4 )
 input_handler.bind('right arrow', 'd')
 input_handler.bind('left arrow', 'a')
 input_handler.bind('up arrow', 'space')
@@ -12,22 +17,29 @@ input_handler.bind('gamepad dpad right', 'd')
 input_handler.bind('gamepad dpad left', 'a')
 input_handler.bind('gamepad a', 'space')
 
-bg = Entity(model='quad',  scale=(200,21), color = color.rgba(0.9,0.7,0.04,1) , z=5)
-bg_2 = Entity(model='quad',  scale=(200,21), z=20,x = 16 )
-ground = Entity(model='quad', y = -7 , scale_x = 30, collider = 'box' , color = color.rgba(0.89, 0.52, 0.5, 0), x = -8 )
+###################################Map Loading########################################################################################################################
+bg = Entity(model='quad',  scale=(16**2,16**2), color = color.rgba(0.9,0.7,0.04,1) , z=5)
+duplicate(bg, x = 50, z = 5 , y = -10)
+
+ground = Entity(model='quad', y = -7 , scale_x = 15, collider = 'box' , color = color.rgba(0.89, 0.52, 0.5, 1), x = -8 )
 wall = Entity(model='quad',color = color.rgba(0.89, 0.52, 0.28, 1),scale = (0.5,9), x = 30, y = 0.1, collider = 'box'  )
-duplicate(wall, x = 30 )
+duplicate(wall, x = -30)
 
-level =  Entity(model='quad', y = -6, scale_x = 12 , collider = 'box' , color = color.rgba(0.89, 0.52, 1, 1), x = 8 )
-duplicate(level, x = 8 )
-duplicate(level, x = 8 )
-player = PlatformerController2d(y=-3 ,scale = (2,2,0) , color =color.white, texture = "assets/sprite.png" , jump_height = 2)
-camera.add_script(SmoothFollow(target= player, offset=[0,3,-30], speed=4))
+level =  Entity(model='quad', y = 2, scale_x = 12 , collider = 'box' , color = color.rgba(0.89, 0.52, 1, 1), x = -18 )
+duplicate(level, x = -20 , y = 2)
+####################################################################################################################################
 
+player = PlatformerController2d(y=-3 ,scale = (2,2,0) , color =color.white, texture = "assets/sprite.png" , jump_height = 10)
+camera.add_script(SmoothFollow(target= player, offset=[0,3,-30], speed=3))
 
-
-
-
+def update() :
+    if player.y < -40 :
+        time.sleep(1)
+        player.x = -3
+        player.y = 3
+    while abs(player.x - platform.x) < 1 :
+        platform.x = platform.x + random.randint(-5,5)
+        time.sleep(2)
 
 app.run()
 
